b14aae559956031f601bf15f923694e11a8c41a0
Karim Bakkali <kbldn@live.co.uk>
2020-01-23 22:22:22 +0000

    Added the breeding Rabbits

--- labs/lab_22 Rabbits breed/Program.cs
+++ labs/lab_22 Rabbits breed/Program.cs
@@ -8,11 +8,54 @@ namespace lab_22_Rabbits_breed
         static void Main(string[] args)
         {
             //Rabbits can now produce a new rabbit once a year
+            int totalRabits = 1;
             var rabbitList = new List<Rabbit>();
+            rabbitList.Add(new Rabbit());
+            rabbitList[0].Age = 0;
+            rabbitList[0].RabbitID = 0;
 
 
+            Breed(rabbitList);
+            rabbitList.ForEach(x => Console.WriteLine($"Rabbit {x.RabbitID} and is {x.Age} years old"));
+
+
+            void Breed(List<Rabbit> rabs)
+            {
+                int rabbitsIn = rabs.Count;
+                if (rabbitsIn < 50)
+                {
+                    for (int i = 0; i <= rabbitsIn; i++)
+                    {
+                        ageRabbits(rabs);
+                        foreach (var item in rabs.ToArray())
+                            
+                            {
+                                
+                                    rabs.Add(new Rabbit());
+                                    rabs[totalRabits].RabbitID = totalRabits;
+                                    rabs[totalRabits].Age = 0;
+                                    totalRabits++;
+                                
+                            }
+                        Breed(rabs);
+                        if (totalRabits > 50)
+                        {
+                            break;
+                        }
+                    }
+                }
+                else Console.WriteLine("Too many RabbitS!!!!!");
+
+
+            }
+            void ageRabbits(List<Rabbit> rabs)
+            {
+                rabs.ForEach(x => x.Age++);
+            }
 
         }
+
+
     }
     class Rabbit
     {
