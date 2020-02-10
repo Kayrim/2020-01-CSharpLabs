4b16e00264fbf38aae6ad5b0126793642fd40a4e
Karim Bakkali <KBakkali@spartaglobal.com>
2020-02-10 11:24:40 +0000

    Broken Database

--- labs/lab_43_entity_core_sql_sqlite/Program.cs
+++ labs/lab_43_entity_core_sql_sqlite/Program.cs
@@ -15,14 +15,24 @@ namespace lab_43_entity_core_sql_sqlite
             // user database
             List<User> users = new List<User>();
             List<Category> categories = new List<Category>();
+            List<Company> companies = new List<Company>();
 
             using (var db = new UserDataBaseContext())
             {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                Category admin = new Category() { CategoryID = 1, CategoryName = "BOSS" };
+
+                Company amazon = new Company() { CompanyName = "Amazon", CompanyID = 1 };
+
+                User tim = new User() { UserName = "Tim", CategoryID = 1, DateOfBirth = DateTime.Now, isValid = true, UserID = 1 , CompanyID = 1 };
+
                 users = db.Users.ToList();
                 categories = db.Categories.ToList();
-                users.ForEach(x => Console.WriteLine(x.UserName));
-                categories.ForEach(x => Console.WriteLine(x.CategoryName));
 
+                Console.WriteLine($"{tim.UserName} is an employee {tim.Company.CompanyName} and is the {tim.Category.CategoryName}");
+                
 
             }
         }
@@ -32,13 +42,14 @@ namespace lab_43_entity_core_sql_sqlite
         //db set map tables to classes
         public DbSet<User> Users { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Company> Companies { get; set; }
     
         //connection string
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
             builder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Users;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             // using the sqlite server we built locally 
-            // builder.UseSqlite(@"C:\2020-01-CSharpLabs\SQLite\test.db");
+            //builder.UseSqlite("Data Source = test.db");
         }
     }
 
@@ -58,6 +69,10 @@ namespace lab_43_entity_core_sql_sqlite
         public bool? isValid { get; set; }
 
         public virtual Category Category { get; set; }
+
+        public int? CompanyID { get; set; }
+
+        public virtual Company Company { get; set; }
     }
 
     public class Category
@@ -73,6 +88,13 @@ namespace lab_43_entity_core_sql_sqlite
         public string CategoryName { get; set; }
 
         public virtual ICollection<User> Users { get; set; }
+    }
+    public class Company
+    {
+
+        public int CompanyID { get; set; }
+        public string CompanyName { get; set; }
+
     }
 }
 
